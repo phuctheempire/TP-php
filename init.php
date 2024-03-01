8ee52c5e8599f55491017e684c18942f0e5abebe
Xuan Phuc PHAM <xuanphuc280203@gmail.com>
2024-03-01 14:14:00 +0100

    a

--- init.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("config.php");
-    session_start();
-
-    require_once(ROOT."app/Database.php"); // Database class
-    require_once(ROOT."app/Controller.php"); // Controller class, behavior of the website
-
-    
